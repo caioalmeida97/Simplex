1375deafcb91873247df9f78e31bb934c26b217f
Caio Almeida <caio.almeida1997@hotmail.com>
2018-09-06 15:29:26 -0300

    Implementing lines and columns constants

--- Simplex/src/simplex/Simplex.java
+++ Simplex/src/simplex/Simplex.java
@@ -3,20 +3,24 @@ package simplex;
 import java.util.Scanner;
 
 public class Simplex {
-
+    
+    
     public static void main(String[] args) {
 //        Scanner s = new Scanner(System.in);
         // z x1 x2 xf1 xf2 xf3 b
         double[][] table = {
-            {1, -3, -5, 0, 0, 0, 0},
-            {0, 2, 4, 1, 0, 0, 10},
-            {0, 6, 1, 0, 1, 0, 20},
-            {0, 1, -1, 0, 0, 1, 30}};
+            {1, -3, -5, 0, 0, 0, 0 },
+            {0,  2,  4, 1, 0, 0, 10},
+            {0,  6,  1, 0, 1, 0, 20},
+            {0,  1, -1, 0, 0, 1, 30}};
 
+        final int LINES = table.length;
+        final int COLUMNS = table[0].length;
+        
         double baseVariable = 0;
         int columnBase = 0;
-        //Determinando a variavel que entrara na base
-        for (int j = 0; j < table[0].length; j++) {
+        // Determinando a variavel que entrara na base
+        for (int j = 0; j < COLUMNS; j++) {
             if(table[0][j] < baseVariable){
                 baseVariable = table[0][j];
                 columnBase = j;
@@ -27,8 +31,8 @@ public class Simplex {
         // Determinar a linha que sai na base
         double pivotD = 9999;
         int lineBase = 0;
-        for(int i = 1; i < table.length; i++){         
-            double aux = table[i][table[0].length - 1]/table[i][columnBase];
+        for(int i = 1; i < LINES; i++){         
+            double aux = table[i][COLUMNS - 1]/table[i][columnBase];
             System.out.println("Aux: " + aux);
             if(aux < pivotD && aux > 0){
                 pivotD = aux;
@@ -38,9 +42,9 @@ public class Simplex {
         }
         
         // Calculando a NLP
-        double[] nlp = new double[table[0].length];
+        double[] nlp = new double[COLUMNS];
         double pivot = table[lineBase][columnBase];
-        for (int j = 0; j < table[lineBase].length; j++) {
+        for (int j = 0; j < COLUMNS; j++) {
             nlp[j] = table[lineBase][j]/pivot;
         }
         
