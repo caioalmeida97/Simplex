c7b2fa56709afae212356ea41486554e0aa70a3d
Caio Almeida <caio.almeida1997@hotmail.com>
2018-09-27 09:32:29 -0300

    Developed simplex method

--- Simplex/src/simplex/Simplex.java
+++ Simplex/src/simplex/Simplex.java
@@ -3,57 +3,108 @@ package simplex;
 import java.util.Scanner;
 
 public class Simplex {
-    
-    
+
     public static void main(String[] args) {
-//        Scanner s = new Scanner(System.in);
         // z x1 x2 xf1 xf2 xf3 b
         double[][] table = {
-            {1, -3, -5, 0, 0, 0, 0 },
-            {0,  2,  4, 1, 0, 0, 10},
-            {0,  6,  1, 0, 1, 0, 20},
-            {0,  1, -1, 0, 0, 1, 30}};
+            {1, -3, -5, 0, 0, 0, 0},
+            {0, 2, 4, 1, 0, 0, 10},
+            {0, 6, 1, 0, 1, 0, 20},
+            {0, 1, -1, 0, 0, 1, 30}};
+        
+        simplex(table);
+    }
 
+    public static void printTable(double[][] table) {
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[0].length; j++) {
+                System.out.print(table[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printLine(double[] line) {
+        for (int i = 0; i < line.length; i++) {
+            System.out.print(line[i] + "\t");
+        }
+    }
+
+    public static boolean greatSolution(double[] line) {
+        for (int i = 0; i < line.length; i++) {
+            if (line[i] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static double[][] simplex(double[][] table) {
         final int LINES = table.length;
         final int COLUMNS = table[0].length;
-        
-        double baseVariable = 0;
-        int columnBase = 0;
-        // Determinando a variavel que entrara na base
-        for (int j = 0; j < COLUMNS; j++) {
-            if(table[0][j] < baseVariable){
-                baseVariable = table[0][j];
-                columnBase = j;
-                System.out.println("Base variable: " + baseVariable + " and column: " + columnBase);
+
+        int countIterations = 0;
+        while (!greatSolution(table[0])) {
+            System.out.println("\nIteration number: " + (countIterations + 1));
+            double baseVariable = 0;
+            int columnBase = 0;
+//            System.out.println("Great solution? " + greatSolution(table[0]));
+
+            // Determinando a variavel que entrara na base
+            for (int j = 0; j < COLUMNS; j++) {
+                if (table[0][j] < baseVariable) {
+                    baseVariable = table[0][j];
+                    columnBase = j;
+                    System.out.println("Base variable: " + baseVariable + " and column: " + columnBase);
+                }
             }
-        }
-        
-        // Determinar a linha que sai na base
-        double pivotD = 9999;
-        int lineBase = 0;
-        for(int i = 1; i < LINES; i++){         
-            double aux = table[i][COLUMNS - 1]/table[i][columnBase];
-            System.out.println("Aux: " + aux);
-            if(aux < pivotD && aux > 0){
-                pivotD = aux;
-                lineBase = i;
-                System.out.println("Pivot determinant: " + pivotD);
+
+            // Determinar a linha que sai na base
+            double pivotD = table[1][COLUMNS - 1] / table[1][columnBase];
+            int lineBase = 1;
+            for (int i = 2; i < LINES; i++) {
+                double aux = table[i][COLUMNS - 1] / table[i][columnBase];
+                System.out.println("Aux: " + aux);
+                if (aux < pivotD && aux > 0) {
+                    pivotD = aux;
+                    lineBase = i;
+                }
             }
+            System.out.println("Pivot determinant: " + pivotD);
+
+            // Calculando a NLP
+            double[] nlp = new double[COLUMNS];
+            double pivot = table[lineBase][columnBase];
+            for (int j = 0; j < COLUMNS; j++) {
+                nlp[j] = table[lineBase][j] / pivot;
+            }
+
+            for (int i = 0; i < nlp.length; i++) {
+                System.out.print(nlp[i] + "\t");
+            }
+            System.out.println("\n");
+
+            // Calcular as outras linhas a partir da nova linha pivot
+            double[][] newTable = table.clone();
+            for (int i = 0; i < LINES; i++) {
+                if (i != lineBase) {
+                    double[] tempNlp = new double[COLUMNS];
+                    for (int k = 0; k < tempNlp.length; k++) {
+                        tempNlp[k] = nlp[k] * (table[i][columnBase] * (-1));
+                    }
+                    for (int j = 0; j < COLUMNS; j++) {
+                        newTable[i][j] = table[i][j] + tempNlp[j];
+                    }
+                } else {
+                    newTable[i] = nlp;
+                }
+            }
+            table = newTable;
+            System.out.println("New Table:");
+            printTable(table);
+            countIterations++;
         }
-        
-        // Calculando a NLP
-        double[] nlp = new double[COLUMNS];
-        double pivot = table[lineBase][columnBase];
-        for (int j = 0; j < COLUMNS; j++) {
-            nlp[j] = table[lineBase][j]/pivot;
-        }
-        
-        for (int i = 0; i < nlp.length; i++) {
-            System.out.print(nlp[i] + "\t");
-        }
-        System.out.println();
-        
-        // Calcular as outras linhas a partir da nova linha pivot
+        return table;
     }
 
 }
